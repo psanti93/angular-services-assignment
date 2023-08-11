8a79ebfdcdf55eed08d356d18353c4d91d848b5e
Paul Santiago <paulsantiago282@gmail.com>
2023-08-11 19:18:57 -0400

    Remove todo's

--- src/app/counter.service.ts
+++ src/app/counter.service.ts
@@ -3,9 +3,7 @@ import { EventEmitter, Injectable } from "@angular/core";
 
 @Injectable()
 export class CounterService {
-  //I want to subscribe whenver an event is emitted on going active -> inactive
-  // inactive - > active
-  // have these be subscribed to by the active in active components respectivel
+
   inactiveCount = 0;
   activeCount = 0;
   activeToInactiveEmit = new EventEmitter<Number>();
